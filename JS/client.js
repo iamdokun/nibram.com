9000cacc1f81dcdfa98647dbd0821223bbca8bd4
iamdokun <dokun.abraham@gmail.com>
2021-04-11 11:31:48 +0100

    new update

--- JS/client.js
+++ JS/client.js
@@ -1,49 +1,7 @@
 // ***************************** //
-// let myFunction = () => {
-//     let replyBtn = document.getElementsByClassName('replyBtn');
-//     let replied = document.getElementsByClassName('replied');
-//     for (let i = 0; i < replyBtn.length; i++) {
-//         for (let j = 0; j < replied.length; j++) {
-//             let img = replied[j];
-//             let button = replyBtn[i]; 
-            
-//             if (button.style.display = 'block') {
-//                 button.style.display = 'none';
-//                 img.style.display = 'block';
-//            }
-//         }
-//     }
-    
-    
-// };
+
 let replyBtn = document.getElementsByClassName('replyBtn');
 let replied = document.getElementsByClassName('replied');
-// let button;
-// let img;
-
-// for (let i = 0; i < replyBtn.length; i++) {
-    
-//     for (let j = 0; j < replied.length; j++) {
-//         let img = replied[j];
-//         let button = replyBtn[i]; 
-//         // console.log(img);
-        
-//         button.addEventListener('click', (e) => {
-//             console.log(e);
-//             if (button.style.display = 'block') {
-//                 button.style.display = 'none';
-//                 // for (let k = 0; k < img.length; k++) {
-//                 //     let current = img[k];
-//                 //     current.style.display = 'block';
-//                 // }
-//                 img.style.display = 'block';
-//            }
-//         });
-
-        
-//     }
-// }
-
 
 for (let i = 0; i < replyBtn.length; i++) {
     
@@ -54,10 +12,6 @@ for (let i = 0; i < replyBtn.length; i++) {
             console.log(e);
             if (button.style.display = 'block') {
                 button.style.display = 'none';
-                // for (let k = 0; k < img.length; k++) {
-                //     let current = img[k];
-                //     current.style.display = 'block';
-                // }
                 replied[i].style.display = 'block';
            }
         });
