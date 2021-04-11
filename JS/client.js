3aa7b3f6ada5af0567d06e5e65c5d459cd5d12e7
iamdokun <dokun.abraham@gmail.com>
2021-04-11 09:02:05 +0100

    new update

--- JS/client.js
+++ JS/client.js
@@ -1,18 +1,67 @@
 // ***************************** //
-let myFunction = () => {
-    let replyBtn = document.getElementsByClassName('replyBtn');
-    let replied = document.getElementsByClassName('replied');
-    for (let i = 0; i < replyBtn.length; i++) {
-        for (let j = 0; j < replied.length; j++) {
-            let img = replied[j];
-            let button = replyBtn[i]; 
+// let myFunction = () => {
+//     let replyBtn = document.getElementsByClassName('replyBtn');
+//     let replied = document.getElementsByClassName('replied');
+//     for (let i = 0; i < replyBtn.length; i++) {
+//         for (let j = 0; j < replied.length; j++) {
+//             let img = replied[j];
+//             let button = replyBtn[i]; 
             
+//             if (button.style.display = 'block') {
+//                 button.style.display = 'none';
+//                 img.style.display = 'block';
+//            }
+//         }
+//     }
+    
+    
+// };
+let replyBtn = document.getElementsByClassName('replyBtn');
+let replied = document.getElementsByClassName('replied');
+// let button;
+// let img;
+
+// for (let i = 0; i < replyBtn.length; i++) {
+    
+//     for (let j = 0; j < replied.length; j++) {
+//         let img = replied[j];
+//         let button = replyBtn[i]; 
+//         // console.log(img);
+        
+//         button.addEventListener('click', (e) => {
+//             console.log(e);
+//             if (button.style.display = 'block') {
+//                 button.style.display = 'none';
+//                 // for (let k = 0; k < img.length; k++) {
+//                 //     let current = img[k];
+//                 //     current.style.display = 'block';
+//                 // }
+//                 img.style.display = 'block';
+//            }
+//         });
+
+        
+//     }
+// }
+
+
+for (let i = 0; i < replyBtn.length; i++) {
+    
+        let button = replyBtn[i]; 
+        // console.log(img);
+        
+        button.addEventListener('click', (e) => {
+            console.log(e);
             if (button.style.display = 'block') {
                 button.style.display = 'none';
-                img.style.display = 'block';
+                // for (let k = 0; k < img.length; k++) {
+                //     let current = img[k];
+                //     current.style.display = 'block';
+                // }
+                replied[i].style.display = 'block';
            }
-        }
-    }
-    
-    
-};
+        });
+
+        
+
+}
